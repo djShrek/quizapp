50e69ce1a513650d0d7c23173aeb67500cafddf5
Robert Ho <boho527@gmail.com>
2013-05-08 23:00:12 -0700

    added back button, start over button, a display score section at the end of the quiz

--- quiz.js
+++ quiz.js
@@ -31,7 +31,9 @@ var a = 0;
 var j = 0;
 var userScore = 0;
 var questionPlace = document.getElementById('quizQuestion');
-
+var buttons = document.getElementsByName("test");
+// var theAnswer = allQuestions[j].answer;
+var keepTrack = [];
 
 /*
 Create "next" button using JavaScript
@@ -47,29 +49,36 @@ Create "next" button using JavaScript
 2. If button checked's value is equal to correctAnswer, userScore++
 */
 function scoreUser(){
-if (j === allQuestions.length){
+if (j === (allQuestions.length - 1)){
 	j = 0;
+	clearQuestion();
+	displayScore();
+	finalDisplay();
 }
 
+// If you click on the submit button and there are no radio buttons selected, alert
+// that they must choose an answer.
+
+
+
 var theAnswer = allQuestions[j].answer;
-var buttons = document.getElementsByName("test");
+
 for (var i = 0; i < buttons.length; i++){
 	if (buttons[i].checked === true) {
 		a++;
-		console.log(theAnswer);
 		if (buttons[i].value === theAnswer.toString()){
 			userScore++;
-			removeButtons();
+			keepTrack.unshift(buttons[i].id);
+			clearQuestion();
 			createQuestion();
-			displayScore();
 			}
 		else {
-
-			removeButtons();
+			clearQuestion();
 			createQuestion();
-			displayScore();
-		}
-		j++;
+			keepTrack.unshift(buttons[i].id);
+			}
+			j++;
+			console.log(userScore);
 		}
 	}
 }
@@ -78,12 +87,13 @@ for (var i = 0; i < buttons.length; i++){
 removeButtons function is used to delete all
 */
 
-function removeButtons(){
-	var allChildren = questionPlace.childNodes;
-		for (var k = 1; k <= allChildren.length; k++){
+function clearQuestion(){
+	questionPlace.innerHTML = "";
+	/*var allChildren = questionPlace.childNodes; // old code, which works but not as efficient
+		for (var k = 0; k <= allChildren.length; k++){
 			k = 0;
-				questionPlace.removeChild(allChildren[k]);
-					}
+				questionPlace.removeChild(allChildren[0]);
+					}*/
 				}
 /*
 Order of events:
@@ -93,7 +103,7 @@ Order of events:
 
 */
 
-function createQuestion(){
+function createQuestion(e){
 if (a === allQuestions.length){
 	a = 0;
 }
@@ -138,18 +148,86 @@ var d = document.createElement('div');
 d.id = "displayScore";
 d.textContent = "Your score is: " + userScore + "/10";
 questionPlace.appendChild(d);
-
 }
-displayScore();
+
+
+
+function finalDisplay(){ // Code to display only the "start over" buttong 
+var back = document.getElementById('back');
+back.style.display  = "none";
+var start = document.getElementById('score');
+start.style.display = "none";
+var startOver = document.getElementById("startOver");
+startOver.style.display = "inline";
+}
+
+/*
+Go back to the previous question; if first question, just return first question.
+*/
+
+function backButton(){
+// have to account for the answers too! 
+if (a <= 0){
+	a++;
+}
+if (j <=0 ){
+	j++;
+}
+else {
+	j--;
+}
+var e = --a;
+clearQuestion();
+createQuestion(e); // create previous question
+var keepTrackOfButton = keepTrack.splice(0,1)[0]; // return last button id
+var answer = allQuestions[j].answer;
+var prevButton = document.getElementById(keepTrackOfButton);
+if (prevButton.value === answer.toString()){ // Have to subtract from userScore if they go back and got the previous question right
+	userScore--;
+}
+prevButton.checked = true;
+console.log(userScore);
+
+
+// buttonSet();
+}
+
+/* function buttonSet(){
+var test = keepTrack.splice(0,1)[0];
+var prevButton = document.getElementById(test);
+userScore--;
+prevButton.checked = true;
+}
+*/
+
+/*
+Code below is basically a "hard" reset, resetting all the variables and clearing
+out all the created HTML. 
+*/
+
+function startOver(){
+console.log(displayScore);
+a = 0;
+j = 0;
+i = 0;
+userScore = 0;
+keepTrack = [];
+var display = document.getElementById('displayScore');
+questionPlace.removeChild(display);
+clearQuestion();
+createQuestion();
+var back = document.getElementById('back');
+back.style.display  = "inline";
+var start = document.getElementById('score');
+start.style.display = "inline";
+var startAgain = document.getElementById('startOver');
+startAgain.style.display = "none";
+}
+
+
+
 // need to make a score box
 // Upload to GitHub
 // Use OOP to clean up code
 // Recreate with jQuery
 // Style 
-
-var testOOP = function(){
-var object1 = {
-newInput: function(){document.createElement('')}
-
-}
-}
